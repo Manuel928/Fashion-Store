f9e2e450001e99568f815a0c9389e9cbc9495e51
Emmanuel Oluwatosin Alao <Afrikareportersng@gmail.com>
2023-10-09 10:42:30 +0100

    fix mobile responsiveness

--- public/app.js
+++ public/app.js
@@ -1,3 +1,32 @@
+// Add this to your existing script.js
+const menuToggle = document.getElementById('menuToggle');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('close');
+
+menuToggle.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Close the modal when the user clicks outside of it
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+
+const goUp = document.querySelector('.go-up')
+goUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
